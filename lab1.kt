462f3b186152031712fb181b9913d786d19dbe28
Yuen Shan Tsang <A00270323@mycambrian.ca>
2023-02-02 01:21:46 -0500

    Added some comments

--- lab1.kt
+++ lab1.kt
@@ -9,37 +9,45 @@ At last it displays the new result.
 */
 
 fun main(){
-    //Get a numeric value from the user
+    //Get a numeric value from user
     print("Please enter a numeric value: ")
     val number = readLine()!!.toFloat()
 
-    print("Convert from km, mi, cm, in, kg, lb, g, oz, C, F, L and cup.\nPlease select and enter the current unit of measurement: ")
+    //Get a unit from user
+    print("Please select the current unit of measurement from km, mi, cm, in, kg, lb, g, oz, C, F, L and cup.\nEnter unit: ")
     val measurementUnit = readLine()
 
     var conversion: String
     when (measurementUnit) {
+        //Convert between km and mi
         "km" -> conversion = "$number$measurementUnit = ${number*0.62}mi"
         "mi" -> conversion = "$number$measurementUnit = ${number*1.61}km"
 
+        //Convert between cm and in
         "cm" -> conversion = "$number$measurementUnit = ${number*0.39}in"
         "in" -> conversion = "$number$measurementUnit = ${number*2.54}cm"
 
+        //Convert between kg and lb
         "kg" -> conversion = "$number$measurementUnit = ${number*2.2}lb"
         "lb" -> conversion = "$number$measurementUnit = ${number*0.45}kg"
 
+        //Convert between g and oz
         "g" -> conversion = "$number$measurementUnit = ${number*0.04}oz"
         "oz" -> conversion = "$number$measurementUnit = ${number*28.35}g"
 
+        //Convert between C and F
         "C" -> conversion = "$number$measurementUnit = ${number*(9/5) + 32}F or ${number + 273.15}K"
-
         "F" -> conversion = "$number$measurementUnit = ${(number-32) * (5/9)}C or ${(number-32) * (5/9) + 273.15}K"
         
+        //Convert between L and cup
         "L" -> conversion = "$number$measurementUnit = ${number*4.17}cups"
         "cup" -> conversion = "$number$measurementUnit = ${number*0.24}L"
 
-        else -> conversion = "Please enter valid unit"
+        //Invalid unit
+        else -> conversion = "Please try again and enter a valid unit"
     }
 
+    //Show the new value
     println(conversion)
 
 }
